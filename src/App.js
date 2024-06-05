5951d94480688e52ce666a98b0356486ba82c1f3
mahongri <mahongri1993@gmail.com>
2024-06-05 00:12:18 -0400

    hook useCallback

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { memo, useState, useCallback} from 'react';
 import './App.css';
 
+const Input = memo(function Input({onChange}){
+  console.log('Input rendered');
+  return <input type= "text" onChange = {(e) => onChange(e.target.value)} />
+})
 function App() {
+  const [count, setCount] = useState(0);
+  const changeHandler = useCallback((value) => {
+    console.log(value);
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Input  onChange = {changeHandler}/>
+      <button onClick = {() => {setCount(count + 1)}}>{count}</button>
     </div>
   );
 }
